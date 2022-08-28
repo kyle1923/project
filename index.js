ab5bd3239a8e2ced892814dc715a6740e882da0a
Kyle Vermillion <kvermillion23@gmail.com>
2022-08-27 20:43:12 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,88 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const userCardTemplate = document.querySelector("[data-user-template]")
+    const userCardContainer = document.querySelector("[data-user-cards-container]")
+    const searchInput = document.querySelector("[data-search]")
+
+    let drink = []
+    // let users = []
+    // searchInput.addEventListener("input", e => {
+    //     const value = e.target.value.toLowerCase()
+
+    //     users.forEach(user => {
+    //         const isVisible = 
+    //         user.name.toLowerCase().includes(value) || 
+    //         user.email.toLowerCase().includes(value)
+    //         user.element.classList.toggle("hide", !isVisible)
+    //     })
+    // })
+
+    // fetch("https://jsonplaceholder.typicode.com/users")
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         users = data.map(user => {
+    //             const card = userCardTemplate.content.cloneNode(true).children[0]
+    //             const header = card.querySelector("[data-header]")
+    //             const body = card.querySelector("[data-body]")
+    //             header.textContent = user.name
+    //             body.textContent = user.email
+    //             userCardContainer.append(card)
+    //             return { name: user.name, email: user.email, element: card }
+    //         })
+    //     })
+    searchInput.addEventListener("input", e => {
+        const value = e.target.value.toLowerCase()
+
+        drink.forEach(user => {
+            const isVisible =
+                user.strDrink.toLowerCase().includes(value) ||
+                user.strGlass.toLowerCase().includes(value)
+            user.element.classList.toggle("hide", !isVisible)
+        })
+    })
+
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+        .then(res => res.json())
+        //.then(data => console.log(data.drinks))
+        .then(data => {
+            drink = data.drinks.map(user => {
+                // data.drinks.forEach(user => {
+                const card = userCardTemplate.content.cloneNode(true).children[0]
+                //  console.log(keys)
+                const header = card.querySelector("[data-header]")
+                const body = card.querySelector("[data-body]")
+                const body1 = card.querySelector("[data-body1]")
+                const body2 = card.querySelector("[data-body2]")
+                
+                const image = card.querySelector("[image]")
+                const ing4 = user.strIngredient4
+               if (ing4 == " "){
+                return ing4 == " "
+               }
+               
+
+                header.textContent = user.strDrink
+                body.textContent = user.strGlass
+
+                
+                     body2.textContent = user.strIngredient1 +" "+  user.strMeasure1 + ", " + user.strIngredient2 + " " + user.strMeasure2 + ", " + " " + user.strIngredient3 + " " + user.strMeasure3
+                    
+                body1.textContent = user.strInstructions
+                image.src = user.strDrinkThumb
+
+                
+
+                userCardContainer.append(card)
+                return {
+                    strDrink: user.strDrink, strGlass: user.strGlass, strInstructions: user.strInstructions, strDrinkThumb: user.strDrinkThumb,
+                    element: card
+                }
+                
+         
+            })
+                
+            
+        })
+
+        
+})
+
