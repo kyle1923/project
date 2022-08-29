0fb737b9a6bf131d1414e1bc7c037e64bda1364a
Kyle Vermillion <kvermillion23@gmail.com>
2022-08-29 14:12:15 -0400

    index changes

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+
         .then(res => res.json())
         //.then(data => console.log(data.drinks))
         .then(data => {
@@ -41,11 +42,11 @@ document.addEventListener('DOMContentLoaded', () => {
                 body1.textContent = user.strInstructions
                 image.src = user.strDrinkThumb
                 header.addEventListener("mouseover", event => {
-                    if (event.target.style.color === "purple"){
+                    if (event.target.style.color === "purple") {
                         event.target.style.color = "black"
-                    } else {event.target.style.color = "purple"}
+                    } else { event.target.style.color = "purple" }
                 })
-            
+
 
 
                 userCardContainer.append(card)
@@ -56,8 +57,64 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
             })
+            
 
-        
+        })
+        let drink1 = []
+    searchInput.addEventListener("input", e => {
+        const value = e.target.value.toLowerCase()
+
+        drink1.forEach(user => {
+            const isVisible =
+                user.strDrink.toLowerCase().includes(value) ||
+                user.strGlass.toLowerCase().includes(value)
+            user.element.classList.toggle("hide", !isVisible)
+        })
+    })
+        fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s")
+
+        .then(res => res.json())
+        //.then(data => console.log(data.drinks))
+        .then(data => {
+            drink1 = data.drinks.map(user => {
+                // data.drinks.forEach(user => {
+                const card = userCardTemplate.content.cloneNode(true).children[0]
+                //  console.log(keys)
+                const header = card.querySelector("[data-header]")
+                const body = card.querySelector("[data-body]")
+                const body1 = card.querySelector("[data-body1]")
+                const body2 = card.querySelector("[data-body2]")
+
+                const image = card.querySelector("[image]")
+
+
+
+                header.textContent = user.strDrink
+                body.textContent = user.strGlass
+
+
+                body2.textContent = user.strIngredient1 + " " + user.strMeasure1 + ", " + user.strIngredient2 + " " + user.strMeasure2
+
+                body1.textContent = user.strInstructions
+                image.src = user.strDrinkThumb
+                header.addEventListener("mouseover", event => {
+                    if (event.target.style.color === "purple") {
+                        event.target.style.color = "black"
+                    } else { event.target.style.color = "purple" }
+                })
+
+
+
+                userCardContainer.append(card)
+                return {
+                    strDrink: user.strDrink, strGlass: user.strGlass, strInstructions: user.strInstructions, strDrinkThumb: user.strDrinkThumb,
+                    element: card
+                }
+
+
+            })
+            
+
         })
 
 
